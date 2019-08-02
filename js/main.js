56956ee48d34f3e229ba13935d0489ddb7e13767
Samantha <sami.shackelford@gmail.com>
2019-08-02 13:53:27 -0700

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,66 @@
+console.log('sanity');
+
+const deck = [
+    {
+        suit: 'heart',
+        value: 13,
+        img:'../images/hearts-K.svg',
+        proceeds:''
+    },
+    {
+        suit: 'heart',
+        value: 12
+    },
+    {
+        suit: 'heart',
+        value: 11
+    },
+    {
+        suit: 'heart',
+        value: 10
+    },
+    {
+        suit: 'heart',
+        value: 9
+    },
+    {
+        suit: 'heart',
+        value: 8
+    },
+    {
+        suit: 'heart',
+        value: 7
+    },
+    {
+        suit: 'heart',
+        value: 6
+    },
+    {
+        suit: 'heart',
+        value: 5
+    },
+    {
+        suit: 'heart',
+        value: 4
+    },
+    {
+        suit: 'heart',
+        value: 3
+    },
+    {
+        suit: 'heart',
+        value: 2
+    },
+    {
+        suit: 'heart',
+        value: 1
+    }
+
+]
+
+//constants of soundbites, card flipping, celebratory winnning sound
+
+
+//State variables --> what you have to keep track of 
+//top card of destination pile 
+//bottom card of moving pile
